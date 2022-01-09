fb7ab21264750cfe23312225840d41d868af3978
Higor Rafael Moura RabÊlo <86304823+higorrabelo@users.noreply.github.com>
2022-01-09 16:47:07 -0300

    Color changes and some modifications

--- Application/Program.java
+++ Application/Program.java
@@ -3,9 +3,8 @@ import chess.ChessMatch;
 
 public class Program {
 
-    public static void main(String args[]){  
-        ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
-    }
-    
+    public static void main(String[] args) {
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+	}
 }
