99653bcb54629a3fb5bc8505ce565a82c337a062
Higor Rafael Moura RabÊlo <86304823+higorrabelo@users.noreply.github.com>
2022-01-09 09:35:27 -0300

    FirstClass: Position

--- Application/Program.java
+++ Application/Program.java
@@ -1,12 +1,17 @@
 package Application;
 import java.util.Scanner;
+
+import boardgame.Position;
 public class Program {
 
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite Algo");
-        String algo = sc.next();
-        System.out.println(algo);
+        System.out.println("Enter the Position Row");
+        int row = sc.nextInt();
+        System.out.println("Enter the Position Column");
+        int column = sc.nextInt();
+        Position pos = new Position(row,column);
+        System.out.println(pos);
         sc.close();
     }
     
---
+++ boardgame/Position.java
@@ -0,0 +1,27 @@
+package boardgame;
+
+public class Position {
+    private int row;
+    private int column;
+    public Position(int row,int column){
+        this.row=row;
+        this.column=column;
+    }
+    public void setRow(int row){
+        this.row=row;
+    }
+    public void setColumn(int column){
+        this.column=column;
+    }
+    public int getRow(){
+        return row;
+    }
+    public int getColumn(){
+        return column;
+    }
+    @Override
+    public String toString(){
+        return "Row: "+row+" Column "+column;
+    }
+
+}
