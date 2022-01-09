a69b7f56e34e4b95ca3bbcc932b6c5acf87a813a
Higor Rafael Moura RabÊlo <86304823+higorrabelo@users.noreply.github.com>
2022-01-09 09:20:43 -0300

    Chess game Projet

---
+++ Application/Program.java
@@ -0,0 +1,13 @@
+package Application;
+import java.util.Scanner;
+public class Program {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite Algo");
+        String algo = sc.next();
+        System.out.println(algo);
+        sc.close();
+    }
+    
+}
