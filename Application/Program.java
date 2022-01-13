8113760871704a3c215758ec4c2e67b6d261c2fe
Higor Rafael Moura RabÊlo <86304823+higorrabelo@users.noreply.github.com>
2022-01-13 10:02:06 -0300

    Promotion Better

--- Application/Program.java
+++ Application/Program.java
@@ -38,7 +38,11 @@ public class Program {
                 }
                 if(chessMatch.getPromoted()!=null){
                     System.out.print("Entre Piece For promotion (B/N/R/Q): ");
-                    String type = sc.nextLine();
+                    String type = sc.nextLine().toUpperCase();
+                    while(!type.equals("B") && !type.equals("N")&& !type.equals("R")&& !type.equals("Q")){
+                    System.out.print("Invalid Value. Enter Piece For promotion (B/N/R/Q): ");
+                    type = sc.nextLine().toUpperCase();
+                    }
                     chessMatch.replacePromotedPiece(type);
                 }
 
